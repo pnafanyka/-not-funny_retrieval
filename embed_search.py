11a75866c45136f17caad23a4299dbd0a0039795
andrejpihtin <pnafanyka@gmail.com>
2025-07-25 17:52:35 +0300

    added lost files

---
+++ embed_search.py
@@ -0,0 +1,46 @@
+from sentence_transformers import SentenceTransformer
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+import os
+import json
+
+class EMBED_SEARCH():
+    '''
+    Класс для поиска через эмбеддинги и косинусное расстояние
+    '''
+    model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
+    #модель выбрана с HuggingFace по встроенным фильтрам
+    with open(f'{os.path.abspath(os.path.dirname(__file__))}/load_from/jokess_short.txt', 'r') as f:
+        loaded_jokes = f.read()
+
+    def __init__(self) -> None:
+        self.joke_embeds = np.load(f'{os.path.abspath(os.path.dirname(__file__))}/load_from/joke_embeddings.npy')
+        self.jokes = json.loads(self.loaded_jokes)
+
+    def search_jokes(self, query: str, n: int) -> list:
+        '''
+        Функция находит n-самых близких шуток к запросу
+        '''
+        query_embed = query_embed = self.model.encode(query)
+        similarities = cosine_similarity(query_embed.reshape(1, -1), self.joke_embeds)
+        top_k_indices = np.argsort(similarities[0])[-n:][::-1]
+
+        output_list = []
+        for id in top_k_indices:
+            output_list.append(self.jokes[id])
+
+        return output_list
+    
+    def cli_search_jokes(self, query: str, n: int) -> list:
+        '''
+        Функция находит n-самых близких шуток к запросу
+        '''
+        query_embed = query_embed = self.model.encode(query)
+        similarities = cosine_similarity(query_embed.reshape(1, -1), self.joke_embeds)
+        top_k_indices = np.argsort(similarities[0])[-n:][::-1]
+
+        output_list = []
+        for id in top_k_indices:
+            output_list.append([self.jokes[id], round(similarities[0][id], 2)])
+
+        return output_list
